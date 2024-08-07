80aa21715f180881dded4356b1c515fea9243225
Ece Akın <akinecee3535@hotmail.com>
2024-08-07 15:59:22 +0300

    Add project files.

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminID { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(10)]
+        public string KullaniciAd { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(10)]
+        public string Sifre { get; set; }
+        [Column(TypeName = "Char")]
+        [StringLength(1)]
+        public string Yetki { get; set; }
+
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/FaturaKalem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class FaturaKalem
+    {
+        [Key]
+        public int FaturaKalemID { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(100)]
+        public string Aciklama { get; set; }
+        public int Miktar { get; set; }
+        public decimal BirimFiyat { get; set; }
+        public decimal Tutar { get; set; }
+        public Faturalar Faturalar { get; set; }
+
+    }
+}
