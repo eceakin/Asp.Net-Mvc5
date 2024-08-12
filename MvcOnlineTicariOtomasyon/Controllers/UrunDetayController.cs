d905be0490034ba6ec73ce974ecd0071bf1e74e7
Ece Akın <akinecee3535@hotmail.com>
2024-08-12 17:33:11 +0300

    12.08.2024 Üçüncü commit

---
+++ MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+
+        Context c = new Context();
+
+        // GET: UrunDetay
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Deger1 = c.Uruns.Where(x=>x.UrunID==1).ToList();
+            cs.Deger2 = c.Detays.Where(y=>y.DetayID==1).ToList();
+
+       //     var degerler = c.Uruns.Where(x=>x.UrunID == 1).ToList();
+            return View(cs);
+        }
+    }
+}
--- MvcOnlineTicariOtomasyon/Models/Siniflar/Context.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Context.cs
@@ -18,6 +18,7 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         public DbSet<Personel> Personels { get; set; }
         public DbSet<SatisHareket> SatisHarekets { get; set; }
         public DbSet<Urun> Uruns { get; set; }
+        public DbSet<Detay> Detays { get; set; }
 
     }
 }
